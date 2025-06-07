2149f963d2a6983b9fd0f3aa3fb14479a53e6687
Julek Kopczewski <julek.kopczewski@gmail.com>
2025-06-07 02:40:26 +0200

    .NETStandard 2.0 support

--- src/ECS/Base/SchemaUtils.cs
+++ src/ECS/Base/SchemaUtils.cs
@@ -18,9 +18,13 @@ internal static class SchemaUtils
         if (Platform.IsUnityRuntime) {
             return true;
         }
+#if NETSTANDARD && !NETSTANDARD2_1_OR_GREATER
+        return true;
+#else
         // Android does not require AOT. No need to register components via: new NativeAOT()
         return System.Runtime.CompilerServices.RuntimeFeature.IsDynamicCodeSupported;
     //  return System.Runtime.CompilerServices.RuntimeFeature.IsDynamicCodeCompiled;
+#endif
     }
     
     [ExcludeFromCodeCoverage]
